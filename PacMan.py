b0382bf6be4df53670bf74e321782de678cdf51c
Lukas-drz <47811332+Lukas-drz@users.noreply.github.com>
2019-05-15 15:57:47 +0200

    Add files via upload

---
+++ PacMan.py
@@ -0,0 +1,168 @@
+﻿#!/usr/bin/python3
+# -*- coding: Utf-8 -*
+import pygame
+from pygame.locals import *
+from classes import *
+from random import *
+import time
+pygame.init()
+
+#Affichage page d'accueil
+fenetre = pygame.display.set_mode((400, 400))
+icone = pygame.image.load("icone.png")
+pygame.display.set_icon(icone)
+pygame.display.set_caption("PacMan ISN La Merci : Lukas Deronzier, Mathieu Dumarcel, Jules Majoulet")
+
+#Déplacement continu
+pygame.key.set_repeat(200, 20)
+derniere_touche = ""
+#variable lors de K_ESCAPE
+em=0
+eh=0
+h=0
+
+#variable pour la boucle
+continuer = 1
+#Début boucle principale
+while continuer:
+    accueil = pygame.image.load("Title screen/title screen.png").convert()
+    fenetre.blit(accueil,(0,0))
+    pygame.display.flip()
+
+    continuer_jeu = 1
+    continuer_accueil = 1
+
+
+    while continuer_accueil == 1:
+
+        pygame.time.Clock()
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                continuer_jeu = 0
+                continuer_accueil = 0
+                continuer = 0
+
+            #Echappe du menu
+            if h==0 and event.type == KEYDOWN and event.key == K_ESCAPE or h==0 and event.type == MOUSEBUTTONDOWN and event.button == 1 and event.pos[0]<108 and event.pos[0]>20 and event.pos[1]<267 and event.pos[1]>232 :
+                echappe_menu = pygame.image.load("Echappe/Echappe.png").convert()
+                fenetre.blit(echappe_menu,(0,133))
+                pygame.display.flip()
+                em=1
+                choix = 0
+
+            #Oui ou Non du Echappe du menu
+            if em==1 and event.type == MOUSEBUTTONDOWN and event.button == 1 and event.pos[0]<147 and event.pos[0]>71 and event.pos[1]<232 and event.pos[1]>201:
+                continuer_jeu = 0
+                continuer_accueil = 0
+                continuer = 0
+
+            if em == 1 and event.type == MOUSEBUTTONDOWN and event.button == 1 and event.pos[0]<332 and event.pos[0]>243 and event.pos[1]<232 and event.pos[1]>201:
+                fenetre.blit(accueil,(0,0))
+                pygame.display.flip()
+                em=0
+
+            #Help pages
+            if event.type == MOUSEBUTTONDOWN and event.button == 1 and event.pos[0]<109 and event.pos[0]>20 and event.pos[1]<225 and event.pos[1]>192:
+                help1=pygame.image.load("Help screen/Help1.png").convert()
+                fenetre.blit(help1,(0,0))
+                pygame.display.flip()
+                h=1
+
+            #Echappe du Help
+            if h==1 and event.type == KEYDOWN and event.key == K_ESCAPE:
+                echappe_help = pygame.image.load("Echappe/Echappe help.png").convert()
+                fenetre.blit(echappe_help,(0,133))
+                pygame.display.flip()
+                eh=1
+
+            #Oui ou Non du Echappe du Help
+            if eh==1 and event.type == MOUSEBUTTONDOWN and event.button == 1 and event.pos[0]<137 and event.pos[0]>72 and event.pos[1]<233 and event.pos[1]>208:
+                fenetre.blit(accueil,(0,0))
+                pygame.display.flip()
+                eh=0
+                h=0
+
+            if eh == 1 and event.type == MOUSEBUTTONDOWN and event.button == 1 and event.pos[0]<322 and event.pos[0]>246 and event.pos[1]<230 and event.pos[1]>209:
+                fenetre.blit(help1,(0,0))
+                pygame.display.flip()
+
+            #Lorsque l'on appuie sur PLAY
+            if event.type == MOUSEBUTTONDOWN and event.button == 1 and event.pos[0]<108 and event.pos[0]>19 and event.pos[1]<184 and event.pos[1]>151:
+                continuer_accueil = 0
+                choix = 'n1'
+
+    if choix != 0:
+        fenetre_jeu = pygame.display.set_mode((810,750))
+        fond = pygame.image.load("Structure/fond.jpg").convert()
+        fenetre_jeu.blit(fond,(0,0))
+        niveau = Niveau(choix)
+        niveau.generer()
+        niveau.afficher(fenetre)
+        Pacman = Perso("Perso/droite.png", "Perso/gauche.png", "Perso/haut.png", "Perso/bas.png", niveau)
+        Ghost_red = Ghost_red("Ghost/Red/droite.png", "Ghost/Red/gauche.png", "Ghost/Red/haut.png", "Ghost/Red/bas.png", niveau)
+        Ghost_pink = Ghost_pink("Ghost/Pink/droite.png", "Ghost/Pink/gauche.png", "Ghost/Pink/haut.png", "Ghost/Pink/bas.png", niveau)
+        Ghost_cyan = Ghost_cyan("Ghost/Cyan/droite.png", "Ghost/Cyan/gauche.png", "Ghost/Cyan/haut.png", "Ghost/Cyan/bas.png", niveau)
+        Ghost_orange = Ghost_orange("Ghost/Orange/droite.png", "Ghost/Orange/gauche.png", "Ghost/Orange/haut.png", "Ghost/Orange/bas.png", niveau)
+
+        Baie = Baie("baie.png", niveau)
+
+    pygame.key.get_pressed()
+
+    while continuer_jeu == 1:
+
+		#Limitation de vitesse de la boucle
+        pygame.time.Clock().tick(30)
+        Ghost_red.ghost_deplacer()
+        Ghost_orange.ghost_deplacer()
+        Ghost_pink.ghost_deplacer()
+        Ghost_cyan.ghost_deplacer()
+
+        for event in pygame.event.get():
+
+            if event.type == QUIT:
+                continuer_jeu = 0
+                continuer = 0
+            elif event.type == KEYDOWN:
+                if event.key == K_RIGHT or event.key == K_d:
+                    Pacman.deplacer('droite')
+                    #derniere_touche = "right"
+                if event.key == K_LEFT or event.key == K_a: #a car pygame est en qwerty, a correspond à q sur un clavier azerty
+                    Pacman.deplacer('gauche')
+                    #derniere_touche = "left"
+                if event.key == K_DOWN or event.key == K_s:
+                    Pacman.deplacer('bas')
+                    #derniere_touche = "down"
+                if event.key == K_UP or event.key == K_w: #w car pygame est en qwerty, w correspond à z sur un clavier azerty
+                    Pacman.deplacer('haut')
+                    #derniere_touche = "up"
+                if event.key == K_ESCAPE or event.key == K_F4 and bool(event.mod & KMOD_ALT):
+                    #echappe_jeu = pygame.image.load("Echappe/Echappe jeu.png").convert()
+                    #fenetre_jeu.blit(echappe_jeu,(690,540))
+                    #pygame.display.flip()
+                    continuer = 0
+                    continuer_accueil = 0
+                    continuer_jeu = 0
+                #while not pygame.key.get_focused():
+                #    if derniere_touche == "right":
+                #        Pacman.deplacer('droite')
+                #    if derniere_touche == "left":
+                #        Pacman.deplacer('gauche')
+                #    if derniere_touche == "down":
+                #        Pacman.deplacer('bas')
+                #    if derniere_touche == "up":
+                #        Pacman.deplacer('haut')
+
+		#Affichages aux nouvelles positions
+        fenetre.blit(fond, (0,0))
+        niveau.afficher(fenetre)
+        fenetre.blit(Baie.direction, (Baie.x, Baie.y))
+        fenetre.blit(Pacman.direction, (Pacman.x, Pacman.y))
+        fenetre.blit(Ghost_red.direction, (Ghost_red.x, Ghost_red.y))
+        fenetre.blit(Ghost_pink.direction, (Ghost_pink.x, Ghost_pink.y))
+        fenetre.blit(Ghost_orange.direction, (Ghost_orange.x, Ghost_orange.y))
+        fenetre.blit(Ghost_cyan.direction, (Ghost_cyan.x, Ghost_cyan.y))
+        pygame.display.flip()
+
+
+pygame.quit()
---
+++ classes.py
@@ -0,0 +1,215 @@
+import pygame
+from pygame.locals import *
+from random import *
+import time
+
+class Niveau:
+
+	def __init__(self, fichier):
+		self.fichier = fichier
+		self.structure = 0
+
+
+	def generer(self):
+
+		#On ouvre le fichier
+		with open(self.fichier, "r") as fichier:
+			structure_niveau = []
+			#On parcourt les lignes du fichier
+			for ligne in fichier:
+				ligne_niveau = []
+				#On parcourt les sprites (lettres) contenus dans le fichier
+				for sprite in ligne:
+					#On ignore les "\n" de fin de ligne
+					if sprite != '\n':
+						#On ajoute le sprite à la liste de la ligne
+						ligne_niveau.append(sprite)
+				#On ajoute la ligne à la liste du niveau
+				structure_niveau.append(ligne_niveau)
+			#On sauvegarde cette structure
+			self.structure = structure_niveau
+
+
+	def afficher(self, fenetre):
+
+		mur = pygame.image.load("Structure/mur.png").convert()
+		design = pygame.image.load("pac_gomme.png").convert_alpha()
+
+		#On parcourt la liste du niveau
+		num_ligne = 0
+		for ligne in self.structure:
+			#On parcourt les listes de lignes
+			num_case = 0
+			for sprite in ligne:
+				#On calcule la position réelle en pixels
+				x = num_case * 30
+				y = num_ligne * 30
+				if sprite == 'm':		   #m = Mur
+					fenetre.blit(mur, (x,y))
+					num_case += 1
+				elif sprite == 'r' or sprite == 'p' or sprite == 'q':
+					fenetre.blit(design, (x,y))
+					num_case += 1
+
+			num_ligne += 1
+
+class Perso:
+	def __init__(self, droite, gauche, haut, bas, niveau):
+		#Sprites du personnage
+		self.droite = pygame.image.load(droite).convert_alpha()
+		self.gauche = pygame.image.load(gauche).convert_alpha()
+		self.haut = pygame.image.load(haut).convert_alpha()
+		self.bas = pygame.image.load(bas).convert_alpha()
+		#Position du personnage en cases et en pixels
+		self.case_x = 14
+		self.case_y = 23
+		self.x = 390
+		self.y = 660
+		#Direction par défaut
+		self.direction = self.droite
+		#Niveau dans lequel le personnage se trouve
+		self.niveau = niveau
+
+
+	def deplacer(self, direction):
+
+		#Déplacement vers la droite
+		if direction == 'droite':
+			if self.niveau.structure[self.case_y][self.case_x+1] == 'p':
+				self.case_x = 0
+			#On vérifie que la case de destination n'est pas un mur
+			elif self.niveau.structure[self.case_y][self.case_x+1] != 'm':
+				#Déplacement d'une case
+				self.case_x += 1
+				#Calcul de la position "réelle" en pixel
+				self.x = self.case_x * 30
+			#Image dans la bonne direction
+			self.direction = self.droite
+
+		#Déplacement vers la gauche
+		if direction == 'gauche':
+			if self.niveau.structure[self.case_y][self.case_x-1] == 'q':
+				self.case_x = 26
+			elif self.niveau.structure[self.case_y][self.case_x-1] != 'm':
+				self.case_x -= 1
+				self.x = self.case_x * 30
+			self.direction = self.gauche
+
+		#Déplacement vers le haut
+		if direction == 'haut':
+			if self.niveau.structure[self.case_y-1][self.case_x] != 'm':
+				self.case_y -= 1
+				self.y = self.case_y * 30
+			self.direction = self.haut
+
+		#Déplacement vers le bas
+		if direction == 'bas':
+			if self.niveau.structure[self.case_y+1][self.case_x] != 'm':
+				self.case_y += 1
+				self.y = self.case_y * 30
+			self.direction = self.bas
+
+class Ghost:
+	def __init__(self, droite, gauche, haut, bas, niveau):
+		#Sprites du personnage
+		self.droite = pygame.image.load(droite).convert_alpha()
+		self.gauche = pygame.image.load(gauche).convert_alpha()
+		self.haut = pygame.image.load(haut).convert_alpha()
+		self.bas = pygame.image.load(bas).convert_alpha()
+
+	def ghost_deplacer(self, direction = 'gauche'):
+		from random import randint
+		a=randint(1,4)
+		if a == 1:
+			if self.niveau.structure[self.case_y-1][self.case_x] != 'm':
+				self.case_y -= 1
+				self.y = self.case_y * 30
+			self.direction = self.haut
+		if a == 2:
+			if self.niveau.structure[self.case_y][self.case_x+1] == 'p':
+				self.case_x = 0
+			elif self.niveau.structure[self.case_y][self.case_x+1] != 'm':
+                #Déplacement d'une case
+				self.case_x += 1
+                #Calcul de la position "réelle" en pixel
+				self.x = self.case_x * 30
+            #Image dans la bonne direction
+			self.direction = self.droite
+		if a == 3:
+			if self.niveau.structure[self.case_y+1][self.case_x] != 'm':
+				self.case_y += 1
+				self.y = self.case_y * 30
+			self.direction = self.bas
+		if a == 4:
+			if self.niveau.structure[self.case_y][self.case_x-1] == 'q':
+				self.case_x = 26
+			elif self.niveau.structure[self.case_y][self.case_x-1] != 'm':
+				self.case_x -= 1
+				self.x = self.case_x * 30
+			self.direction = self.gauche
+		
+class Ghost_red(Ghost):
+	def __init__(self, droite, gauche, haut, bas, niveau):
+		self.droite = pygame.image.load(droite).convert_alpha()
+		self.gauche = pygame.image.load(gauche).convert_alpha()
+		self.haut = pygame.image.load(haut).convert_alpha()
+		self.bas = pygame.image.load(bas).convert_alpha()
+		self.case_x = 12
+		self.case_y = 12
+		self.x = 330
+		self.y = 330
+		self.direction = self.gauche
+		self.niveau = niveau
+		Ghost.ghost_deplacer(self)
+
+class Ghost_pink(Ghost):
+	def __init__(self, droite, gauche, haut, bas, niveau):
+		self.droite = pygame.image.load(droite).convert_alpha()
+		self.gauche = pygame.image.load(gauche).convert_alpha()
+		self.haut = pygame.image.load(haut).convert_alpha()
+		self.bas = pygame.image.load(bas).convert_alpha()
+		self.case_x = 12
+		self.case_y = 14
+		self.x = 330
+		self.y = 390
+		self.direction = self.bas
+		self.niveau = niveau
+		Ghost.ghost_deplacer(self)
+
+class Ghost_cyan(Ghost):
+	def __init__(self, droite, gauche, haut, bas, niveau):
+		self.droite = pygame.image.load(droite).convert_alpha()
+		self.gauche = pygame.image.load(gauche).convert_alpha()
+		self.haut = pygame.image.load(haut).convert_alpha()
+		self.bas = pygame.image.load(bas).convert_alpha()
+		self.case_x = 16
+		self.case_y = 12
+		self.x = 450
+		self.y = 330
+		self.direction = self.haut
+		self.niveau = niveau
+		Ghost.ghost_deplacer(self)
+
+class Ghost_orange(Ghost):
+	def __init__(self, droite, gauche, haut, bas, niveau):
+		self.droite = pygame.image.load(droite).convert_alpha()
+		self.gauche = pygame.image.load(gauche).convert_alpha()
+		self.haut = pygame.image.load(haut).convert_alpha()
+		self.bas = pygame.image.load(bas).convert_alpha()
+		self.case_x = 16
+		self.case_y = 14
+		self.x = 450
+		self.y = 390
+		self.direction = self.droite
+		self.niveau = niveau
+		Ghost.ghost_deplacer(self)
+
+class Baie:
+	def __init__(self, design, niveau):
+		self.design = pygame.image.load(design).convert_alpha()
+		self.niveau = niveau
+		self.case_x = 2
+		self.case_y = 2
+		self.x = 30
+		self.y = 30
+		self.direction = self.design
